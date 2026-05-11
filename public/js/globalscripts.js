ff5b0df7a3e4763b8794a12ed5b97ff632d15e58
harol <haroldgaelcardenastrejo@gmail.com>
2026-05-10 23:32:09 -0600

    Modificando rutas relativas de scripts .js

--- public/js/globalscripts.js
+++ public/js/globalscripts.js
@@ -21,7 +21,7 @@ function togglePassword() {
     const type = password_field.type === "password" ? "text" : "password";
     password_field.type = type;
 
-    const eye_icon = type === "password" ? "../img/eyeclosed.png" : "../img/eyeopened.png";
+    const eye_icon = type === "password" ? "http://127.0.0.1:8000/img/eyeclosed.png" : "http://127.0.0.1:8000/img/eyeopened.png";
 
     document.querySelector("span[onclick='togglePassword()'] > img").src = eye_icon;
 }
@@ -262,7 +262,7 @@ function previewImage(event, id_img, id_form = "") {
             alert("El archivo seleccionado no es una imagen.");
             fileinput.value = ""; //Vaciamos el FileList para que el usuario pueda seleccionar otro archivo válido.
             if (id_form == "update") window.location.reload();
-            else preview_img.src = "../img/default-avatar.png";
+            else preview_img.src = "http://127.0.0.1:8000/img/default-avatar.png";
             return;
         }
 
@@ -277,7 +277,7 @@ function previewImage(event, id_img, id_form = "") {
             alert('Ocurrió un error al cargar la imagen. Por favor, inténtelo de nuevo.');
             fileinput.value = ""; //Vaciamos el FileList para que el usuario pueda seleccionar otro archivo válido.
             if (id_form == "update") window.location.reload();
-            else preview_img.src = "../img/default-avatar.png";
+            else preview_img.src = "http://127.0.0.1:8000/img/default-avatar.png";
         }
 
         reader.readAsDataURL(file);
@@ -285,7 +285,7 @@ function previewImage(event, id_img, id_form = "") {
 
     else {
         if (id_form == "update") window.location.reload();
-        else preview_img.src = "../img/default-avatar.png";
+        else preview_img.src = "http://127.0.0.1:8000/img/default-avatar.png";
     }
 }
 
@@ -320,7 +320,7 @@ async function cancelRegistration() {
 
             if (response.ok) {
                 alert(data.message);
-                window.location.href = '../html/login.html';
+                window.location.href = 'http://127.0.0.1:8000/';
                 return;
             }
             else {
@@ -423,7 +423,7 @@ function deletePhoto(event, id_img) {
             return;
         }
 
-        document.getElementById(id_img).src = "../img/default-avatar.png";
+        document.getElementById(id_img).src = "http://127.0.0.1:8000/img/default-avatar.png";
 
         event.target.setAttribute("disabled", "");
 
